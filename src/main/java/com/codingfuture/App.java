fe6241ef747f8c593d782b7b0a0694d126605dbc
wangchen <2805268151@qq.com>
2021-09-09 19:03:58 +0800

    second commit

--- src/main/java/com/codingfuture/App.java
+++ src/main/java/com/codingfuture/App.java
@@ -7,14 +7,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("spring.xml");
-        City city = (City) classPathXmlApplicationContext.getBean("city2");
+        ClassPathXmlApplicationContext cpac = new ClassPathXmlApplicationContext("spring1.xml");
+        //City city = (City) cpac.getBean("city2");
 //        city.test();
-        System.out.println(city.getName());
+        //System.out.println(city.getName());
 //        City city2 = (City) classPathXmlApplicationContext.getBean("city2");
 //        city2.test();
 //        City city3 = (City) classPathXmlApplicationContext.getBean("city3");
 //        city3.test();
-
+        //Person person = (Person)cpac.getBean("person");
+        //System.out.println(person.getCity().getName());
+        //Person person = (Person)cpac.getBean("person1");
+        //System.out.println(person.getMap().get("name"));
+        Object bean = cpac.getBean("person");
     }
 }
--- src/main/java/com/codingfuture/Person.java
+++ src/main/java/com/codingfuture/Person.java
@@ -1,8 +1,26 @@
 package com.codingfuture;
 
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+@Component(value = "person")
 public class Person {
     private String sex;
     private String name;
+    private City city;
+    private Map<String,String> map;
+
+    public Person() {
+        System.out.println("无参构造");
+    }
+
+    public Person(String sex, String name, City city, Map<String, String> map) {
+        this.sex = sex;
+        this.name = name;
+        this.city = city;
+        this.map = map;
+        System.out.println("有参构造");
+    }
 
     public String getSex() {
         return sex;
@@ -19,4 +37,21 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Map<String, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+
 }
